e4e6aad68c1b068d9fb8eec3cbf2ac06c80fa302
にしゅう <37263474+nisyuu@users.noreply.github.com>
2026-02-07 14:21:13 +0900

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -38,7 +39,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-3R2VC90NS2"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3R2VC90NS2');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
